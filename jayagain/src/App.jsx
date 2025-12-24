745664a7d11fa371197f32cc24c7356c73deb2b9
jay-pambhar <pambharjay.prolix@gmail.com>
2025-12-24 11:16:56 +0530

    changes from new prac

--- jayagain/src/App.jsx
+++ jayagain/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Jay + React</h1>
+      <h1>Jay (from newprac) + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
